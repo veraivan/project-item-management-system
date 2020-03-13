b88dfe460c777d27e6d5b5fa636e2b9bcd3b9f83
Giuliano Gonzalez <giuli1297.gg@gmail.com>
2020-03-13 18:47:22 -0300

    Se agrego el database a usar al settings.py

--- ProjectManager/settings.py
+++ ProjectManager/settings.py
@@ -62,13 +62,17 @@ WSGI_APPLICATION = 'ProjectManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
