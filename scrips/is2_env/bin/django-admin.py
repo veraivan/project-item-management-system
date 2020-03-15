4d2f0b18db4cca0240ec92760d98dd992a6d26e4
Cesar Gonzalez <giuli1297.gg@gmail.com>
2020-03-15 15:33:52 -0300

    Se termino la parte del script que setea el ambiente de desarrollo

---
+++ scrips/is2_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/giuli/PycharmProjects/IS2/project-item-management-system/scrips/is2_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
