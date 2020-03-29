4228e63614f0993ee65dfcd1ff8bf6c20b500082
Giuliano <giuli1297.gg@gmail.com>
2020-03-29 00:32:45 -0400

    Agregados algunos cambios a lso requierements

--- loginAuth/urls.py
+++ loginAuth/urls.py
@@ -4,8 +4,8 @@ from loginAuth import views
 
 urlpatterns = [
     path('', views.index),
-    path('dashboard', views.dashboard),
-    path('logout', views.logout),
+    path('dashboard/', views.dashboard),
+    path('logout/', views.logout),
     path('', include('django.contrib.auth.urls')),
     path('', include('social_django.urls')),
 ]
