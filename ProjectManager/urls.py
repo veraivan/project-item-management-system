a2e21a6e82791a80b59c784ee1d8ff48526f7982
Giuliano <giuli1297.gg@gmail.com>
2020-03-28 20:32:39 -0400

    Algunos convenciones de django asumidads

--- ProjectManager/urls.py
+++ ProjectManager/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from loginAuth import urls as loginauth_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('loginAuth.urls')),
+    path('', include(loginauth_urls)),
 ]
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -12,22 +12,18 @@ sudo apt-get install libpq-dev
 echo "INSTALACION"
 sudo apt-get install virtualenv
 echo "CREACIONN"
-virtualenv -p /usr/bin/python3.6 is2_env
+virtualenv -p /usr/bin/python3.6 ../../is2_env
 
 echo "\n\n\nACTIVACION\n\n\n"
 #ln -s is2_env/bin/activate
-source ./is2_env/bin/activate
+source ../../../is2_env/bin/activate
 pip list
 pip install -r requirements.txt
 pip list
 cd project-item-management-system
-<<<<<<< HEAD
 git checkout desarrolloPruebaGiuli
 sudo -u postgres psql -c '\x' -c "ALTER USER postgres WITH PASSWORD 'postgres';"
-#psql -c '\x' -c "ALTER USER postgres WITH PASSWORD '12345';"
 #deactivate
 #rm -rf is2_env/
-=======
-sudo -u postgres psql -c '\x' -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
->>>>>>> e13e650958cc4f04655f013dc5a92bc33fd584d8
+
