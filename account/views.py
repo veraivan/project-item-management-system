9e7f7262aeb850bc25f4ae8b1a82f682b27765e4
Ivan <ivera2065@fpuna.edu.py>
2020-04-03 00:53:18 -0400

    se agregaron nuevas funciones

--- account/views.py
+++ account/views.py
@@ -20,11 +20,10 @@ class Registro(View):
         form = UsuarioForm(request.POST)
         if form.is_valid():
             form.save()
-            id_usuario = ProjectUser.objects.get(form.username).id
+            id_usuario = ProjectUser.objects.get(username=form.cleaned_data['username']).id
             conexion = conectarSSO()
             guardarUsuarioSSO(conexion, form.cleaned_data, id_usuario)
-            valor = True
-        return render(request, 'account/index.html', {'mensaje_valido': valor})
+        return render(request, 'account/index.html')
 
 def conectarSSO():
     dominio = 'authentication-django.auth0.com'
@@ -48,7 +47,7 @@ def guardarUsuarioSSO(conexion, usuario, idUsuario):
         'email': usuario['email'],
         'password': usuario['password'],
         'nickname': usuario['username'],
-        'user_id': idUsuario
+        'user_id': str(idUsuario),
     })
 
 def actualizarUsuarioSSO(conexion, id, usuario):
